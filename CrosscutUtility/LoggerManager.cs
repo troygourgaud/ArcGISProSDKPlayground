64fe6e6822999cbf40c305678727e049dbca70d7
SERVICES\taung <taung@esriaustralia.com.au>
2019-08-30 12:22:27 +0800

    add nlogging sample
    add nlogging sample

---
+++ CrosscutUtility/LoggerManager.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrosscutUtility
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private ILogger logger;
+        //https://stackoverflow.com/questions/3000653/using-nlog-as-a-rollover-file-logger
+        //configuration information
+        public LoggerManager(string nLogConfigLocation)
+        {
+            LogManager.LoadConfiguration(nLogConfigLocation);
+            this.logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public void LogDebug(string message)
+        {
+            this.logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            this.logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            this.logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            this.logger.Warn(message);
+        }
+    }
+}
---
+++ firstAddOn-ArcgisPro/UIButton/BtnTestLogging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace firstAddOn_ArcgisPro.UIButton
+{
+    internal class BtnTestLogging : Button
+    {
+        protected override void OnClick()
+        {
+            Module1.Current.ModuleLogManager.LogDebug("Test debug");
+            Module1.Current.ModuleLogManager.LogError("Test error log");
+            Module1.Current.ModuleLogManager.LogInfo("Test infoLog");
+            Module1.Current.ModuleLogManager.LogWarn("Test warning log");
+        }
+    }
+}
